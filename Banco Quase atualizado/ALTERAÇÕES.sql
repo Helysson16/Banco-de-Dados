33d709f5b3db22f927b3d6c9ea981125da432494
PEPO1235 <131263826+PEPO1235@users.noreply.github.com>
2023-09-22 11:32:30 -0300

    Update ALTERAÇÕES.sql

--- Banco Quase atualizado/ALTERAÇÕES.sql
+++ Banco Quase atualizado/ALTERAÇÕES.sql
@@ -2,3 +2,4 @@ alter table empresa add email varchar (50), add CEP varchar(30), add telefone va
 alter table usuario add foto varchar(10);
 alter table meta add mes_meta varchar(10);
 alter table administrador add nome varchar (50);
+alter table notificacao add nome varcahr (50), add data varchar(50);
